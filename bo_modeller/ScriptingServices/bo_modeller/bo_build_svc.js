0ca82f21af3b5b26773b2e7ab6746b5aa97b3217
shturec <sthrakal@gmail.com>
2016-10-07 21:58:13 +0300

    with build dialog now

--- bo_modeller/ScriptingServices/bo_modeller/bo_build_svc.js
+++ bo_modeller/ScriptingServices/bo_modeller/bo_build_svc.js
@@ -25,44 +25,24 @@ function handleRequest() {
 	} else {
 		//read, parse and validate input
 		var input = request.readInputText();
-	    var items = JSON.parse(input);
+	    var buildRequest = JSON.parse(input);
 	    
-	    if(items){
+	    if(buildRequest && buildRequest.entities){
 	    
-	    	for(var i=0; i< items.length; i++){
-				var tableDef = {
-				  "templateType":"table",
-				  "fileName": items[i].boh_name.replace(/\s+/g, ''),
-				  "projectName":"testproject",
-				  "packageName":"mypackage1"
-				};
-				tableDef.columns = [];
-				if(items[i].properties){
-					for(var j=0; j< items[i].properties.length; j++){
-						var prop = items[i].properties[j];
-						tableDef.columns.push({
-							  "name": prop.boi_name,
-							  "type": prop.boi_type,
-						      "length": prop.boi_length,
-						      "notNull": prop.boi_null,
-						      "primaryKey": prop.boi_pk,
-						      "defaultValue": prop.boi_default||'null'
-						  });
-					}
-				}
-				console.info(tableDef);
-	    		var worker = generator.getWorker(generator.WORKER_CATEGORY_DATA_STRUCTURES);
-				try{	    		
-					worker.generate(tableDef);				
-				} catch(err){
-				    response.setStatus(500);
-				    response.println('{"err": '+(err.message || err)+'}');
-					response.flush();
-					response.close();
-					return;
-				}					
+	    	for(var i=0; i< buildRequest.entities.length; i++){
+	    		var entity = buildRequest.entities[i];
+	    		if(buildRequest.ds === true){
+	    			try{
+	    				buildDataStructures(buildRequest.projectName, buildRequest.packageName, entity);
+					} catch(err){
+					    response.setStatus(500);
+					    response.println('{"err": '+(err.message || err)+'}');
+						response.flush();
+						response.close();
+						return;
+					}						    				
+    			}				
 			}
-			//response.println('{"status":"ok"}');
     	}  else {
     		response.setStatus(400);
     		response.println('{"err":"No input data for build operation provided"}');
@@ -76,4 +56,30 @@ function handleRequest() {
 	response.flush();
 	response.close();
 
+};
+
+function buildDataStructures(project, packageName, entity){
+	var tableDef = {
+	  "templateType":"table",
+	  "fileName": entity.boh_name.replace(/\s+/g, ''),
+	  "projectName":project,
+	  "packageName":packageName
+	};
+	tableDef.columns = [];
+	if(entity.properties){
+		for(var j=0; j< entity.properties.length; j++){
+			var prop = entity.properties[j];
+			tableDef.columns.push({
+				  "name": prop.boi_name,
+				  "type": prop.boi_type,
+			      "length": prop.boi_length,
+			      "notNull": prop.boi_null,
+			      "primaryKey": prop.boi_pk,
+			      "defaultValue": prop.boi_default||'null'
+			  });
+		}
+	}
+	console.info(tableDef);
+	var worker = generator.getWorker(generator.WORKER_CATEGORY_DATA_STRUCTURES);
+	return worker.generate(tableDef);
 };
