371fa3f4eea0be619ace354e0b9b2f31f145125d
shturec <sthrakal@gmail.com>
2016-10-10 17:56:08 +0300

    full stack app gen avail. now

--- bo_modeller/ScriptingServices/bo_modeller/bo_build_svc.js
+++ bo_modeller/ScriptingServices/bo_modeller/bo_build_svc.js
@@ -3,8 +3,7 @@
 
 var request = require('net/http/request');
 var response = require('net/http/response');
-var xss = require("utils/xss");
-var generator = require('gen/generator');
+var generator = require('platform/generator');
 
 handleRequest();
 
@@ -26,25 +25,45 @@ function handleRequest() {
 		//read, parse and validate input
 		var input = request.readInputText();
 	    var buildRequest = JSON.parse(input);
+	    var ds_worker, svc_worker, web_worker;
 	    
 	    if(buildRequest && buildRequest.entities){
 	    
 	    	for(var i=0; i< buildRequest.entities.length; i++){
 	    		var entity = buildRequest.entities[i];
-	    		if(buildRequest.ds === true){
-	    			try{
-	    				buildDataStructures(buildRequest.projectName, buildRequest.packageName, entity);
-					} catch(err){
-					    response.setStatus(500);
-					    response.println('{"err": '+(err.message || err)+'}');
-						response.flush();
-						response.close();
-						return;
-					}						    				
-    			}				
+	    		var template = getBaseTemplate(buildRequest.projectName, buildRequest.packageName, entity);
+	    		try{	    		
+		    		if(buildRequest.ds === true){
+		    			if(!ds_worker)
+		    				ds_worker = generator.getWorker(generator.WORKER_CATEGORY_DATA_STRUCTURES);
+	    				var generatorInput = JSON.parse(JSON.stringify(template));		    				
+		    			generateDataStructure(entity, generatorInput, ds_worker);
+	    			}
+	    			if(buildRequest.svc === true){
+		    			if(!svc_worker)
+		    				svc_worker = generator.getWorker(generator.WORKER_CATEGORY_SCRIPTING_SERVICES);
+	    				var generatorInput = JSON.parse(JSON.stringify(template));			    				
+		    			generateService(entity, generatorInput, svc_worker);	    			
+	    			}
+	    			if(buildRequest.web === true){
+		    			if(!web_worker)
+		    				web_worker = generator.getWorker(generator.WORKER_CATEGORY_WEB_CONTENT_FOR_ENTITY);
+		    			var generatorInput = JSON.parse(JSON.stringify(template));
+		    			generateUIForEntity(entity, generatorInput, web_worker);	    			
+	    			}				
+				} catch(err){
+				    response.setStatus(500);
+				    console.error(err.message || err);
+				    console.error(err.stack);
+				    response.println('{"err": '+(err.message || err)+'}');
+					response.flush();
+					response.close();
+					return;
+				}						    				
 			}
     	}  else {
     		response.setStatus(400);
+    		console.error('No input data for build operation provided');
     		response.println('{"err":"No input data for build operation provided"}');
 			response.flush();
 			response.close();
@@ -58,28 +77,100 @@ function handleRequest() {
 
 };
 
-function buildDataStructures(project, packageName, entity){
-	var tableDef = {
-	  "templateType":"table",
-	  "fileName": entity.boh_name.replace(/\s+/g, ''),
-	  "projectName":project,
-	  "packageName":packageName
+
+function getBaseTemplate(projectName, packageName, entity){
+	var baseTemplate = {
+		"projectName":projectName,
+		"packageName":packageName,
+	  	"fileName": entity.boh_name.replace(/\s+/g, '_')
 	};
-	tableDef.columns = [];
+	baseTemplate.columns = [];	
+	return baseTemplate;
+}
+
+function generateDataStructure(entity, template, worker){
+	template.templateType = 'table';
+	template.fileName = template.fileName + '.table';
 	if(entity.properties){
 		for(var j=0; j< entity.properties.length; j++){
 			var prop = entity.properties[j];
-			tableDef.columns.push({
-				  "name": prop.boi_name,
-				  "type": prop.boi_type,
-			      "length": prop.boi_length,
-			      "notNull": prop.boi_null,
-			      "primaryKey": prop.boi_pk,
-			      "defaultValue": prop.boi_default||'null'
-			  });
+			prop = createSQLEntity(prop);			
+			prop.visible = prop.boi_visible || true;
+			template.columns.push(prop);
+		}
+	}	
+	console.log('[Table Template Source]:' + template);
+	return worker.generate(template);
+};
+
+function generateService(entity, template, worker){
+	template.templateType = "js_db_crud";
+	template.tableName =  template.fileName;
+	template.fileName = template.fileName + '.js';	
+	if(entity.properties){
+		for(var j=0; j< entity.properties.length; j++){
+			var prop = entity.properties[j];
+			prop = createSQLEntity(prop);			
+			prop.visible = prop.boi_visible || true;
+			template.columns.push(prop);
 		}
 	}
-	console.info(tableDef);
-	var worker = generator.getWorker(generator.WORKER_CATEGORY_DATA_STRUCTURES);
-	return worker.generate(tableDef);
+	console.log('[Service Template Source]:' + template);
+	return worker.generate(template);
+}
+
+function generateUIForEntity(entity, template, worker){
+	template.templateType = "list_and_manage";
+	template.pageTitle =  template.fileName;
+	template.tableName =  template.fileName;
+	template.serviceEndpoint = '../js/'+template.packageName+'/'+template.fileName+'.js';	
+	template.fileName = template.fileName + '.html';
+	if(entity.properties){
+		for(var j=0; j< entity.properties.length; j++){
+			var prop = entity.properties[j];
+			prop = createSQLEntity(prop);		
+			prop.visible = prop.boi_visible || true;			
+			prop.label = prop.boi_label || prop.name;
+			prop.widgetType = prop.boi_widgetType || 'text';
+			prop.size = prop.boi_size || prop.label.length;
+			template.columns.push(prop);
+		}
+	}	
+	console.log('[UI Template Source]:' + template);
+	return worker.generate(template);
 };
+
+//Prepare a JSON object for insert into DB
+function createSQLEntity(item) {
+	var persistentItem = {
+		length: item.boi_length || 0,
+		notNull: item.boi_null || true,
+		primaryKey: item.boi_pk || false,
+		defaultValue: item.boi_default || ''
+	};
+	persistentItem.name = item.boi_name.replace(/\s+/g, '_');
+	persistentItem.type = stringToCodeItemTypeMapping(item.boi_type);
+	if(persistentItem.length === 0 && persistentItem.type === 'VARCHAR'){
+		persistentItem.length = 255;
+	}
+	if(persistentItem.defaultValue === undefined){
+		persistentItem.defaultValue = 'null';
+	}	
+	console.debug("Transformation to DB JSON object finished: " + persistentItem);
+	return persistentItem;
+};
+
+function stringToCodeItemTypeMapping(typeIndex) {
+	if(!isNaN(parseInt(typeIndex))){
+		return typeIndex;
+	}
+	if(typeIndex === 'Integer')
+		return 'INTEGER';
+	if(typeIndex === 'String')
+		return 'VARCHAR';
+	if(typeIndex === 'Boolean')
+		return 'TINYINT';
+	if(typeIndex === 'Relationship')
+		return 'OBJECT';
+}
+
--- bo_modeller/WebContent/bo_modeller/js/app.builddialog.js
+++ bo_modeller/WebContent/bo_modeller/js/app.builddialog.js
@@ -1,15 +1,24 @@
 angular.module('businessObjects')
-.controller('BuildDialogCtrl', ['masterDataSvc', 'BuildService', '$scope', function(masterDataSvc, BuildService, $scope) {
+.controller('BuildDialogCtrl', ['masterDataSvc', 'BuildService', '$scope', '$log', '$stateParams', '$window', function(masterDataSvc, BuildService, $scope, $log, $stateParams, $window) {
 
 	this.cfg = {};
 	var self = this;
 	this.build = function(){
 		var buildRequestPayload = angular.copy(self.cfg);
 		buildRequestPayload.entities = masterDataSvc.getLoadedData();
-		BuildService.build(buildRequestPayload);
-		$scope.$close();
+		try{
+			BuildService.build(buildRequestPayload);
+		} catch(err){
+			$log.debug('The requested application path ' + $window.location.href + " is not valid.");
+			$stateParams.message = {
+				text: $window.location.href + ' is not valid application path. Check the URL and try again.',
+				type: 'alert-danger'
+			};
+		}finally{
+			$scope.$close();
+		}
 	};
 	this.cancel = function(){
 		$scope.$dismiss();
 	};
-}])
+}]);
