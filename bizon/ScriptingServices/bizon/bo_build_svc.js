c55504386d4767e7313c3f6adbb3a1ccfb4a89b6
shturec <sthrakal@gmail.com>
2016-10-19 16:32:53 +0300

    full stack app generation now respects entity configuration

--- bizon/ScriptingServices/bizon/bo_build_svc.js
+++ bizon/ScriptingServices/bizon/bo_build_svc.js
@@ -31,25 +31,26 @@ function handleRequest() {
 	    
 	    	for(var i=0; i< buildRequest.entities.length; i++){
 	    		var entity = buildRequest.entities[i];
-	    		var template = getBaseTemplate(buildRequest.projectName, buildRequest.packageName, entity);
-	    		try{	    		
+	    		var baseTemplate = getBaseTemplate(buildRequest.projectName, buildRequest.packageName, entity);
+	    		var template = JSON.parse(JSON.stringify(baseTemplate));//copy
+	    		try{	
 		    		if(buildRequest.ds === true){
 		    			if(!ds_worker)
 		    				ds_worker = generator.getWorker(generator.WORKER_CATEGORY_DATA_STRUCTURES);
-	    				var generatorInput = JSON.parse(JSON.stringify(template));		    				
-		    			generateDataStructure(entity, generatorInput, ds_worker);
+//	    				var generatorInput = JSON.parse(JSON.stringify(template));		    				
+		    			generateDataStructure(entity, template, ds_worker);
 	    			}
 	    			if(buildRequest.svc === true){
 		    			if(!svc_worker)
 		    				svc_worker = generator.getWorker(generator.WORKER_CATEGORY_SCRIPTING_SERVICES);
-	    				var generatorInput = JSON.parse(JSON.stringify(template));			    				
-		    			generateService(entity, generatorInput, svc_worker);	    			
+//	    				var generatorInput = JSON.parse(JSON.stringify(template));			    				
+		    			generateService(entity, template, svc_worker);	    			
 	    			}
 	    			if(buildRequest.web === true){
 		    			if(!web_worker)
 		    				web_worker = generator.getWorker(generator.WORKER_CATEGORY_WEB_CONTENT_FOR_ENTITY);
-		    			var generatorInput = JSON.parse(JSON.stringify(template));
-		    			generateUIForEntity(entity, generatorInput, web_worker);	    			
+//		    			var generatorInput = JSON.parse(JSON.stringify(template));
+		    			generateUIForEntity(entity, template, web_worker);	    			
 	    			}				
 				} catch(err){
 				    response.setStatus(500);
@@ -85,58 +86,83 @@ function getBaseTemplate(projectName, packageName, entity){
 	  	"fileName": entity.boh_name.replace(/\s+/g, '_')
 	};
 	baseTemplate.columns = [];	
+	if(entity.properties){
+		for(var j=0; j< entity.properties.length; j++){
+			var prop = entity.properties[j];
+			prop = createSQLEntity(prop);
+			baseTemplate.columns.push(prop);
+		}
+	}
+	var id = createEntityIDColumnDef(entity);
+	baseTemplate.columns.push(id);		
 	return baseTemplate;
 }
 
 function generateDataStructure(entity, template, worker){
+	if(!entity.boh_ds_gen_enabled)
+		return;
 	template.templateType = 'table';
-	template.fileName = template.fileName + '.table';
-	if(entity.properties){
-		for(var j=0; j< entity.properties.length; j++){
-			var prop = entity.properties[j];
-			prop = createSQLEntity(prop);			
-			prop.visible = prop.boi_visible || true;
-			template.columns.push(prop);
-		}
-	}	
+	template.fileName = entity.boh_table + '.table';
 	console.log('[Table Template Source]:' + template);
 	return worker.generate(template);
-};
+}
+
+function lengthBySQLType(type){
+	if(type === 2){
+		return 255;
+	} 
+	return 0;
+}
+
+function defaultValueBySQLType(type){
+	if(2 === type){
+		return '';
+	} 
+	return 0;
+}
+
+function createEntityIDColumnDef(entity){
+	var id_type = 'INTEGER';//TODO: Fix this
+	return {
+		name: entity.boh_id_name || 'id',	
+		type: id_type || 'INTEGER',
+		primaryKey: true,	
+		length: entity.boi_length || lengthBySQLType(id_type),
+		notNull: true,
+		defaultValue: defaultValueBySQLType(id_type),
+	};
+}
 
 function generateService(entity, template, worker){
+	if(!entity.boh_svc_gen_enabled)
+		return;
 	template.templateType = "js_db_crud";
-	template.tableName =  template.fileName;
-	template.fileName = template.fileName + '.js';	
-	if(entity.properties){
-		for(var j=0; j< entity.properties.length; j++){
-			var prop = entity.properties[j];
-			prop = createSQLEntity(prop);			
-			prop.visible = prop.boi_visible || true;
-			template.columns.push(prop);
-		}
-	}
+	template.tableName = entity.boh_table;			
+	template.fileName = entity.boh_svc_name + '.js';
+	template.tableType = "table";	
+	template.columns = template.columns.map(function(column){
+		column.visible = column.boi_visible || true;	
+		return column;
+	});	
 	console.log('[Service Template Source]:' + template);
 	return worker.generate(template);
 }
 
 function generateUIForEntity(entity, template, worker){
+	if(!entity.boh_ui_gen_enabled)
+		return;
 	template.templateType = "list_and_manage";
-	template.pageTitle =  template.fileName;
-	template.tableName =  template.fileName;
-	template.serviceEndpoint = '../js/'+template.packageName+'/'+template.fileName+'.js';	
-	template.fileName = template.fileName + '.html';
-	if(entity.properties){
-		for(var j=0; j< entity.properties.length; j++){
-			var prop = entity.properties[j];
-			prop = createSQLEntity(prop);		
-			prop.visible = prop.boi_visible || true;			
-			prop.label = prop.boi_label || prop.name;
-			prop.widgetType = prop.boi_widgetType || 'text';
-			prop.size = prop.boi_size || prop.label.length;
-			template.columns.push(prop);
-		}
-	}	
-	console.log('[UI Template Source]:' + template);
+	template.pageTitle = entity.boh_ui_title || entity.boh_name;
+	template.tableName = template.fileName;
+	template.serviceEndpoint = '/'+template.packageName+'/'+template.fileName;	
+	template.fileName = template.fileName.replace('.js','') + '.html';
+	template.columns = template.columns.map(function(column){
+		column.label = column.boi_label || column.name;
+		column.widgetType = column.boi_widgetType || 'text';
+		column.size = column.boi_size || column.label.length;
+		return column;
+	});	
+	console.info('[UI Template Source]:' + template);
 	return worker.generate(template);
 };
 
@@ -145,7 +171,7 @@ function createSQLEntity(item) {
 	var persistentItem = {
 		length: item.boi_length || 0,
 		notNull: item.boi_null || true,
-		primaryKey: item.boi_pk || false,
+		primaryKey: false,
 		defaultValue: item.boi_default || ''
 	};
 	persistentItem.name = item.boi_name.replace(/\s+/g, '_');
@@ -154,7 +180,7 @@ function createSQLEntity(item) {
 		persistentItem.length = 255;
 	}
 	if(persistentItem.defaultValue === undefined){
-		persistentItem.defaultValue = 'null';
+		persistentItem.defaultValue = '';
 	}	
 	console.debug("Transformation to DB JSON object finished: " + persistentItem);
 	return persistentItem;
